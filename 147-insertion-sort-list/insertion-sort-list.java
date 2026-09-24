a3dfb5dc5c2558798a7e9a43e95033f66e5e2b5a
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-24 09:47:45 +0530

    Clean up comments in insertionSortList method
    Remove unnecessary comments in insertion sort implementation.

--- 147-insertion-sort-list/insertion-sort-list.java
+++ 147-insertion-sort-list/insertion-sort-list.java
@@ -2,7 +2,3 @@ class Solution {
   public ListNode insertionSortList(ListNode head) {
     ListNode dummy = new ListNode(0);
     ListNode prev = dummy; // the last and thus largest of the sorted list
-
-    while (head != null) {       // the current inserting node
-      ListNode next = head.next; // Cache the next inserting node.
-      if (prev.val >= head.val)
