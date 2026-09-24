9b336a06678fa7525a5fabb366397a51fa44e42a
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-24 09:42:29 +0530

    Refactor list splitting logic in sort-list.py
    Removed unnecessary lines for splitting the list.

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -6,11 +6,4 @@ class Solution:
         # Step 1. Split the list to two halves
         prev, slow, fast = None, head, head
         while fast and fast.next:
-            prev = slow
-            slow = slow.next
-            fast = fast.next.next
-            
-        prev.next = None  # Break the list
-        
-        # Step 2. Recursively sort each half
-     
+          
