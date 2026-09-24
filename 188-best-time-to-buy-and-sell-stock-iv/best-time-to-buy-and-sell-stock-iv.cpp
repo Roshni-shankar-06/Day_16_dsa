1c40fe458c78f34bec5d686d49d26350a91c06d5
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-24 09:41:07 +0530

    Start implementing maxProfit function in Solution class

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,7 +1,2 @@
-class Solution {
-public:
-    int maxProfit(int k, vector<int>& prices) {
-        if (k >= prices.size() / 2) {
-            int sell = 0;
-            int hold = INT_MIN;
+
          
