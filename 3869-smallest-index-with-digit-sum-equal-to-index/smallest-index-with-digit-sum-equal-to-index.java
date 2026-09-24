bec08e1c27f5aacf7235317302c595fb6a1e6aef
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-24 09:36:44 +0530

    Remove index checking logic in smallestIndex method
    Removed the logic for checking digit sum equality and returning the index.

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
@@ -1,11 +1,4 @@
 class Solution {
     public int smallestIndex(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            if (getDigitSum(nums[i]) == i) {
-                return i; // Returns the smallest index immediately
-            }
-        }
-        return -1;
-    }
-
-  
+      
