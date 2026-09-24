251501d72d0d714dce9d6a4da045433d701b23ff
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-24 09:38:40 +0530

    Update word-search-ii.py

--- 212-word-search-ii/word-search-ii.py
+++ 212-word-search-ii/word-search-ii.py
@@ -29,13 +29,4 @@ class Solution:
             
             for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 nr, nc = r + dr, c + dc
-                if 0 <= nr < m and 0 <= nc < n and board[nr][nc] != '#' and board[nr][nc] in curr_node.children:
-                    dfs(nr, nc, curr_node)
-                    
-            board[r][c] = char # Restore
-            
-            # Optimization: prune leaf nodes to speed up future searches
-            if not curr_node.children:
-                parent.children.pop(char)
-                
-     
+               
