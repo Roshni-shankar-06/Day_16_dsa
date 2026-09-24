46aeb4d6b1b2073aa200b06078eb1ece7db56ae9
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-24 09:52:31 +0530

    Update linked-list-cycle-ii.java

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -8,8 +8,4 @@ class Solution {
       fast = fast.next.next;
       if (slow == fast) {
         slow = head;
-        while (slow != fast) {
-          slow = slow.next;
-          fast = fast.next;
-        }
-  
+    
