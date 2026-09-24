019db194f2285bdb6757c8c898c92ed08df1611e
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-24 09:42:44 +0530

    Remove base case check for empty list in sortList

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -1,9 +1,3 @@
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        
-        # Step 1. Split the list to two halves
-        prev, slow, fast = None, head, head
-        while fast and fast.next:
-          
+     
