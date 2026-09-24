77d77b4004069f0def5165b9939058f332119873
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-24 09:52:40 +0530

    Refactor cycle detection logic in linked list

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -4,8 +4,4 @@ class Solution {
     ListNode fast = head;
 
     while (fast != null && fast.next != null) {
-      slow = slow.next;
-      fast = fast.next.next;
-      if (slow == fast) {
-        slow = head;
-    
+   
