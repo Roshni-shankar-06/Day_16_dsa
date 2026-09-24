310d46def4518ba3ffbc913649ef1d3a3bb756a6
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-24 09:43:50 +0530

    Add merge sort implementation for linked list
    Implement merge sort for linked list in sortList method.

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -1,1 +1,32 @@
-
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        # Step 1. Split the list to two halves
+        prev, slow, fast = None, head, head
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+            
+        prev.next = None  # Break the list
+        
+        # Step 2. Recursively sort each half
+        l1 = self.sortList(head)
+        l2 = self.sortList(slow)
+        
+        # Step 3. Merge sorted l1 and l2
+        return self.merge(l1, l2)
+        
+    def merge(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = tail = ListNode(None)
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next, l1 = l1, l1.next
+            else:
+                tail.next, l2 = l2, l2.next
+            tail = tail.next
+        
+        tail.next = l1 or l2
+        return dummy.next
