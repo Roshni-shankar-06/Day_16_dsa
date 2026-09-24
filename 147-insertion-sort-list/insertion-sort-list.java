c0079e8d396fd60ffdfb2a7894dabe516d3a2f78
Roshni-shankar-06 <roshnishankar06@gmail.com>
2026-09-24 09:47:56 +0530

    Remove initial insertion sort implementation
    Removed the initial implementation of the insertion sort algorithm.

--- 147-insertion-sort-list/insertion-sort-list.java
+++ 147-insertion-sort-list/insertion-sort-list.java
@@ -1,4 +1,1 @@
-class Solution {
-  public ListNode insertionSortList(ListNode head) {
-    ListNode dummy = new ListNode(0);
-    ListNode prev = dummy; // the last and thus largest of the sorted list
+
